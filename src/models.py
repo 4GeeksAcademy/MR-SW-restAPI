ff2a3b212ad0e80165068c9142c874f32485466b
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2024-09-26 20:27:04 +0000

    generadas tables en base a modelado anterior

--- src/models.py
+++ src/models.py
@@ -7,6 +7,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    firstname = db.Column(db.String, nullable=False)
+    lastname = db.Column(db.String, nullable=False)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +19,106 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname
+
             # do not serialize the password, its a security breach
+        }
+    
+    class Planet(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(100), nullable=False)
+        climate = db.Column(db.String(50))
+        terrain = db.Column(db.String(50))
+        population = db.Column(db.Integer)
+
+        #relationships
+
+        favorites = db.relationship('favorite', back_populates='user')
+
+        def serialize(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+                "climate": self.climate,
+                "terrain": self.terrain,
+                "population": self.population
+            }
+        
+    class Character(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(100), nullable=False)
+        birth_year = db.Column(db.String(50))
+        gender = db.Column(db.String(50))
+        species = db.Column(db.String(50))
+
+        def serialize(self):
+            return{
+                "id": self.id,
+                "name": self.name,
+                "birth_year": self.birth_year,
+                "gender": self.gender,
+                "species": self.species
+            }
+    
+    class Vehicle(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(100), nullable=False)
+        model = db.Column(db.String(100))
+        manufacturer = db.Column(db.String(100))
+        crew = db.Column(db.Integer)
+        passengers = db.Column(db.Integer)
+
+        def serialize(self):
+            return{
+                "id": self.id,
+                "name": self.name,
+                "model": self.model,
+                "manufacturer": self.manufacturer,
+                "crew": self.crew,
+                "passengers": self.passengers,
+            }
+    
+    class Movie(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(100), nullable=False)
+        release_date = db.Column(db.String(20))
+        director = db.Column(db.String(100))
+        producer = db.Column(db.String(100))
+        episode_number = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "director": self.director,
+            "producer": self.producer,
+            "episode_number": self.episode_number
+        }
+    class Favorite(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+        character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+        vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=True)
+        movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=True)
+
+    #relationships
+
+    user = db.relationship('User')
+    planet = db.relationship('Planet')
+    character = db.relationship('Character')
+    vehicle = db.relationship('Vehicle')
+    movie = db.relationship('Movie')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+            "vehicle_id": self.vehicle_id,
+            "movie_id": self.movie_id
         }
